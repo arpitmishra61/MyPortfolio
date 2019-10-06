7a3270599fcc7b8ec5f2af319cb314ecdc307619
Arpit Mishra <arpitmishra61@gmail.com>
2019-10-07 02:21:43 +0530

    first commit

---
+++ navbar.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+    let navs = document.querySelectorAll(".nav-link")
+    navs.forEach((m) => {
+        m.addEventListener('click', (e) => {
+            navs.forEach((m) => {
+                if (m.classList.contains('active')) {
+                    m.classList.remove('active')
+
+                }
+            })
+
+            e.target.classList.add('active')
+
+        })
+    })
+    let home = $("#home");
+    let about = $("#about");
+    let skills = $("#skills");
+    let projects = $("#projects");
+
+    let achievements = $("#achievements");
+    let contact = $("#contact")
+    let footer = $("#footer")
+    let rc;
+    let navheight = $('.navbar').outerHeight()
+    console.log(home.offset())
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= home.offset().top - navheight && window.scrollY < about.offset().top - navheight && !navs[0].classList.contains('active')) {
+            navs.forEach((m) => {
+                if (m.classList.contains('active')) {
+                    rc = m;
+                }
+            })
+            rc.classList.remove('active');
+            navs[0].classList.add('active')
+
+        }
+        else
+
+
+            if (window.scrollY >= about.offset().top - navheight && window.scrollY < skills.offset().top - navheight && !navs[1].classList.contains('active')) {
+                navs.forEach((m) => {
+                    if (m.classList.contains('active')) {
+                        rc = m;
+                    }
+                })
+                rc.classList.remove('active');
+                navs[1].classList.add('active')
+
+            }
+
+            else
+                if (window.scrollY >= skills.offset().top - navheight && window.scrollY < projects.offset().top - navheight && !navs[2].classList.contains('active')) {
+                    navs.forEach((m) => {
+                        if (m.classList.contains('active')) {
+                            rc = m;
+                        }
+                    })
+                    rc.classList.remove('active');
+                    navs[2].classList.add('active')
+
+                }
+                else
+                    if (window.scrollY >= projects.offset().top - navheight && window.scrollY < achievements.offset().top - navheight && !navs[3].classList.contains('active')) {
+                        navs.forEach((m) => {
+                            if (m.classList.contains('active')) {
+                                rc = m;
+                            }
+                        })
+                        rc.classList.remove('active');
+                        navs[3].classList.add('active')
+
+                    }
+                    else
+                        if (window.scrollY >= achievements.offset().top - navheight && window.scrollY < contact.offset().top - navheight && !navs[4].classList.contains('active')) {
+                            navs.forEach((m) => {
+                                if (m.classList.contains('active')) {
+                                    rc = m;
+                                }
+                            })
+                            rc.classList.remove('active');
+                            navs[4].classList.add('active')
+
+                        }
+                        else
+                            if (window.scrollY >= contact.offset().top - navheight && !navs[5].classList.contains('active')) {
+                                navs.forEach((m) => {
+                                    if (m.classList.contains('active')) {
+                                        rc = m;
+                                    }
+                                })
+                                rc.classList.remove('active');
+                                navs[5].classList.add('active')
+
+                            }
+
+
+    })
+
+});
+
+
+
+
+
+
+
+
---
+++ precache-manifest.29523195cbdd7906588fed3386981500.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90007013ea6e950691e5dd1dfb9c9e63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d8d917e43725163031b",
+    "url": "/static/css/main.607c652d.chunk.css"
+  },
+  {
+    "revision": "f35ed9100bf06d33b338",
+    "url": "/static/js/2.6559ce75.chunk.js"
+  },
+  {
+    "revision": "7d8d917e43725163031b",
+    "url": "/static/js/main.63d60e76.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  },
+  {
+    "revision": "b1dfbe43f2c28838a4173f3d29b19bf0",
+    "url": "/static/media/admin.b1dfbe43.png"
+  },
+  {
+    "revision": "5209029d9446783a2472fa38a59cdcc8",
+    "url": "/static/media/bootstrap.5209029d.png"
+  },
+  {
+    "revision": "a4d0aa875abd43961ed7fe051c8f0217",
+    "url": "/static/media/cpp_logo.a4d0aa87.png"
+  },
+  {
+    "revision": "347098113cffb94d36323b473e47f676",
+    "url": "/static/media/image.34709811.jpeg"
+  },
+  {
+    "revision": "7a20defbbdfe7a8163762518e853552c",
+    "url": "/static/media/mongoDB.7a20defb.png"
+  },
+  {
+    "revision": "98c7a94734d2a553a013116f3a200535",
+    "url": "/static/media/profile.98c7a947.jpeg"
+  },
+  {
+    "revision": "6bfdab2e3ed1ae682b4716a62f208bf9",
+    "url": "/static/media/programmer (1).6bfdab2e.png"
+  },
+  {
+    "revision": "12305a23ac320639bac702c7b4443f1d",
+    "url": "/static/media/programmer1.12305a23.png"
+  },
+  {
+    "revision": "2260eb1a0f48eb82d60fd7493339a9f7",
+    "url": "/static/media/react.2260eb1a.png"
+  }
+]);
